e94e4f1de9bc3be2203ddb4ae1e4e1e43c5fc30f
chayouhyeon <krece4658@gmail.com>
2023-02-17 09:41:07 +0900

    article list 시 게시글의 유무 판단

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,30 +1,41 @@
 package com.KoreaIT.java.AM;
+
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-
 		Scanner sc = new Scanner(System.in);
 
 		int lastArticleId = 0;
+
+		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 
 			System.out.printf("명령어 ) ");
 			String command = sc.nextLine();
-
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
-
 			if (command.equals("system exit")) {
 				break;
 			}
 
 			if (command.equals("article list")) {
 				System.out.println("게시글이 없습니다");
+				if (articles.size() == 0) {
+					System.out.println("게시글이 없습니다");
+//					continue;
+				} else {
+					System.out.println("게시글 있던데?");
+				}
+
 			} else if (command.equals("article write")) {
+				int id = lastArticleId + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
@@ -33,6 +44,10 @@ public class Main {
 //				System.out.printf("%s / %s\n", title, body);
 
 				System.out.println((lastArticleId + 1) + "번 글이 생성되었습니다");
+				Article article = new Article(id, title, body);
+				articles.add(article);
+
+				System.out.println(id + "번 글이 생성되었습니다");
 				lastArticleId++;
 			} else {
 
@@ -40,8 +55,20 @@ public class Main {
 
 			}
 		}
-
 		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+
+	Article(int id, String title, String body) {
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+
 }
