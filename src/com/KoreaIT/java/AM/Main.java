1eaa3ebf56ff46473537813a2caa7339ea92408a
chayouhyeon <krece4658@gmail.com>
2023-02-17 09:44:50 +0900

    게시물 목록 기능 구현

--- src/com/KoreaIT/java/AM/Main.java
+++ src/com/KoreaIT/java/AM/Main.java
@@ -1,22 +1,19 @@
 package com.KoreaIT.java.AM;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
 		Scanner sc = new Scanner(System.in);
-
 		int lastArticleId = 0;
-
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
 
 			System.out.printf("명령어 ) ");
-			String command = sc.nextLine();
+			String command = sc.nextLine().trim();
+
 			if (command.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
@@ -24,14 +21,17 @@ public class Main {
 			if (command.equals("system exit")) {
 				break;
 			}
-
 			if (command.equals("article list")) {
-				System.out.println("게시글이 없습니다");
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
 //					continue;
 				} else {
 					System.out.println("게시글 있던데?");
+					System.out.println("  번호   /   제목  ");
+					for (int i = articles.size() - 1; i >= 0; i--) {
+						Article article = articles.get(i);
+						System.out.printf("  %d    /   %s  \n", article.id, article.title);
+					}
 				}
 
 			} else if (command.equals("article write")) {
@@ -43,32 +43,29 @@ public class Main {
 
 //				System.out.printf("%s / %s\n", title, body);
 
-				System.out.println((lastArticleId + 1) + "번 글이 생성되었습니다");
 				Article article = new Article(id, title, body);
 				articles.add(article);
 
 				System.out.println(id + "번 글이 생성되었습니다");
 				lastArticleId++;
 			} else {
-
 				System.out.println("존재x");
 
+				System.out.println("존재하지 않는 명령어 입니다");
 			}
 		}
+
 		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
 }
-
 class Article {
 	int id;
 	String title;
 	String body;
-
 	Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
 		this.body = body;
 	}
-
 }
